d3d79596dff5dbe182fb2a74ddcd6f2aeb03b021
Bonnie-wy <waiyeewong89@gmail.com>
2022-03-10 21:45:17 +0000

    updated indentation

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -18,13 +18,13 @@ export const List: React.FC<IProps> = ({ people }) => {
     return people.map((person) => {
       return (
         <li className="List">
-        <div className="List-header">
-          <img className="List-img" src={person.url} alt={person.name} />
-          <h2>{person.name}</h2>
-        </div>
-          <p>{person.id}</p>
-          <p className="List-note">{person.position}</p>
-      </li>
+          <div className="List-header">
+            <img className="List-img" src={person.url} alt={person.name} />
+            <h2>{person.name}</h2>
+          </div>
+            <p>{person.id}</p>
+            <p className="List-note">{person.position}</p>
+        </li>
       )
     })
   }
